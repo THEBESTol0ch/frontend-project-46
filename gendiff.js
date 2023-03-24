b6fc3e0863bc88a9f120470f0ea6c0e42257ea98
the_bestol0ch <the_bestol0ch@mail.ru>
2023-03-24 21:04:11 +1000

    Step 2

--- gendiff.js
+++ gendiff.js
@@ -3,8 +3,9 @@
 const { program } = require('commander');
 
 program
+	.usage('[options] <filepath1> <filepath2>')
 	.description('Compares two configuration files and shows a difference.')
 	.option('-V, --version', 'output the version number')
-	.helpOption('-h, --help', 'display help for command');
-
-program.parse();
+	.helpOption('-h, --help', 'display help for command')
+	.option('-f, --format <type>', 'output format')
+	.parse(process.argv);
