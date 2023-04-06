ad172c94e220e8d16a3e26818967db3b1ab5d172
the_bestol0ch <the_bestol0ch@mail.ru>
2023-04-06 17:43:34 +1000

    Step 3

--- gendiff.js
+++ gendiff.js
@@ -9,3 +9,32 @@ program
 	.helpOption('-h, --help', 'display help for command')
 	.option('-f, --format <type>', 'output format')
 	.parse(process.argv);
+
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const [path1, path2] = args;
+
+const file1 = JSON.parse(fs.readFileSync(path1));
+const file2 = JSON.parse(fs.readFileSync(path2));
+
+const output = {};
+
+for (const key in file1) {
+  if (!file2.hasOwnProperty(key)) {
+    output[`- ${key}`] = file1[key];
+  } else if (JSON.stringify(file1[key]) !== JSON.stringify(file2[key])) {
+    output[`- ${key}`] = file1[key];
+    output[`+ ${key}`] = file2[key];
+  } else {
+    output[`  ${key}`] = file1[key];
+  }
+}
+
+for (const key in file2) {
+  if (!file1.hasOwnProperty(key)) {
+    output[`+ ${key}`] = file2[key];
+  }
+}
+
+console.log(output);
