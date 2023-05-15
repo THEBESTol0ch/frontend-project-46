b4f4e2a1dcf2dc40e35cc403580e92c570d4e010
THEBESTol0ch <105137450+THEBESTol0ch@users.noreply.github.com>
2023-05-15 15:15:46 +1000

    Step 6

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,38 @@
 const gendiff = require("../src/gendiff.js");
 
-const expectedOutput = {
+const expectedOutput1 = {
   '  host': 'hexlet.io',
   '- timeout': 50,
   '+ timeout': 20,
   '- proxy': '123.234.53.22',
   '- follow': false,
   '+ verbose': true,
+};
+
+const expectedOutput2 = {
+  '  common': {
+    '+ follow': false,
+    '  setting1': 'Value 1',
+    '- setting2': 200,
+    '- setting3': true,
+    '+ setting3': null,
+    '+ setting4': 'blah blah',
+    '+ setting5': { key5: 'value5' },
+    '  setting6': {
+      '  doge': { '- wow': '', '+ wow': 'so much' },
+      '  key': 'value',
+      '+ ops': 'vops'
+    }
+  },
+  '  group1': {
+    '- baz': 'bas',
+    '+ baz': 'bars',
+    '  foo': 'bar',
+    '- nest': { key: 'value' },
+    '+ nest': 'str'
+  },
+  '- group2': { abc: 12345, deep: { id: 45 } },
+  '+ group3': { deep: { id: { number: 45 } }, fee: 100500 }
 };
 
 describe('gendiff', () => {
@@ -17,7 +43,7 @@ describe('gendiff', () => {
       '__tests__/fixtures/file1.json',
       '__tests__/fixtures/file2.json'
     ];
-    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput);
+    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput1);
   });
 
   it('should output correct difference between two yaml files', () => {
@@ -27,7 +53,7 @@ describe('gendiff', () => {
       '__tests__/fixtures/file1.yaml',
       '__tests__/fixtures/file2.yml'
     ];
-    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput);
+    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput1);
   });
 
   it('should output correct difference between two cross format files', () => {
@@ -37,7 +63,7 @@ describe('gendiff', () => {
       '__tests__/fixtures/file1.yaml',
       '__tests__/fixtures/file2.json'
     ];
-    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput);
+    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput1);
   });
 
   it('should return an empty object for one JSON file', () => {
@@ -67,5 +93,15 @@ describe('gendiff', () => {
       '__tests__/fixtures/file1.json'
     ];
     expect(gendiff(fakeArgv.slice(2))).toEqual({});
+  });
+
+  it('should output correct difference between two large json files', () => {
+    const fakeArgv = [
+      'B:\\Roblox\\nodejs\\node.exe',
+      'D:\\Chmod\\bruh\\some\\path',
+      '__tests__/fixtures/large-file1.json',
+      '__tests__/fixtures/large-file2.json'
+    ];
+    expect(gendiff(fakeArgv.slice(2))).toEqual(expectedOutput2);
   });
 });
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,34 +1,65 @@
 const doParse = require("../parsers/parser1.js");
 
+function isObject(value) {
+    return value !== null && typeof value === 'object' && !Array.isArray(value);
+}
+
+function compareObjects(obj1, obj2) {
+    const output = {};
+
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    const allKeys = Array.from(new Set([...keys1, ...keys2])).sort();
+
+    for (const key of allKeys) {
+        const value1 = obj1[key];
+        const value2 = obj2[key];
+
+        if (!obj2.hasOwnProperty(key)) {
+            output[`- ${key}`] = value1;
+        } else if (!obj1.hasOwnProperty(key)) {
+            output[`+ ${key}`] = value2;
+        } else if (isObject(value1) && isObject(value2)) {
+            const nestedOutput = compareObjects(value1, value2);
+            if (Object.keys(nestedOutput).length > 0) {
+                output[`  ${key}`] = nestedOutput;
+            }
+        } else if (JSON.stringify(value1) !== JSON.stringify(value2)) {
+            output[`- ${key}`] = value1;
+            output[`+ ${key}`] = value2;
+        } else {
+            output[`  ${key}`] = value1;
+        }
+    }
+
+    return output;
+}
+
+function compareFiles(file1, file2) {
+    if (isObject(file1) && isObject(file2)) {
+        return compareObjects(file1, file2);
+    } else {
+        return {
+            '-': file1,
+            '+': file2
+        };
+    }
+}
+
 function gendiff(info) {
     let output = {};
 
     if (info[0] == undefined || info[1] == undefined) return output;
     if (info[0] == info[1]) return output;
+
     const file1 = doParse(info[0]);
     const file2 = doParse(info[1]);
 
     if (file1 !== undefined && file2 !== undefined) {
-        for (const key in file1) {
-            if (!file2.hasOwnProperty(key)) {
-                output[`- ${key}`] = file1[key];
-            } else if (JSON.stringify(file1[key]) !== JSON.stringify(file2[key])) {
-                output[`- ${key}`] = file1[key];
-                output[`+ ${key}`] = file2[key];
-            } else {
-                output[`  ${key}`] = file1[key];
-            }
-        }
-
-        for (const key in file2) {
-            if (!file1.hasOwnProperty(key)) {
-                output[`+ ${key}`] = file2[key];
-            }
-        }
-
-        console.log(output);
+        output = compareFiles(file1, file2);
     }
 
+    console.dir(output, {depth: 15});
     return output;
 }
 
